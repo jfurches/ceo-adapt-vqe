736bb000d204cb6db4dd3c97a4e74a9ffbd43671
jfurches <jim.furches@gmail.com>
2024-10-14 10:59:46 -0400

    add error handling in data generation

--- examples/DVG_CEO.py
+++ examples/DVG_CEO.py
@@ -11,6 +11,7 @@ from pathlib import Path
 from adaptvqe import molecules
 from adaptvqe.algorithms.adapt_vqe import LinAlgAdapt
 from adaptvqe.pools import DVG_CEO
+import numpy as np
 
 
 def main(mol_name: str, r: float):
@@ -20,27 +21,39 @@ def main(mol_name: str, r: float):
 
     logfile = Path("logs") / f"{mol_name}-{r:.3f}.txt"
     logfile.parent.mkdir(parents=True, exist_ok=True)
-    # Redirect all the print statements to our log file
-    with open(logfile, "w") as f:
-        with contextlib.redirect_stdout(f):
-            molecule = mol_factory(r)
-            pool = DVG_CEO(molecule)
 
-            my_adapt = LinAlgAdapt(
-                pool=pool,
-                molecule=molecule,
-                max_adapt_iter=1000,
-                recycle_hessian=True,
-                tetris=True,
-                verbose=True,
-                threshold=1e-3,
-                convergence_criterion="max_g",
-            )
-            my_adapt.run()
-            data = my_adapt.data
+    try:
+        # Redirect all the print statements to our log file
+        with open(logfile, "w") as f:
+            with contextlib.redirect_stdout(f):
+                molecule = mol_factory(r)
+                pool = DVG_CEO(molecule)
 
-            # Access the final ansatz indices and coefficients
-            print("Evolution of ansatz indices: ", data.evolution.indices)
+                my_adapt = LinAlgAdapt(
+                    pool=pool,
+                    molecule=molecule,
+                    max_adapt_iter=1000,
+                    recycle_hessian=True,
+                    tetris=True,
+                    verbose=True,
+                    threshold=1e-3,
+                    convergence_criterion="max_g",
+                )
+                my_adapt.run()
+                data = my_adapt.data
+
+                # Access the final ansatz indices and coefficients
+                print("Evolution of ansatz indices: ", data.evolution.indices)
+    except np.linalg.LinAlgError:
+        # Delete the log file so we only have good data samples.
+        # This happens in pyscf I guess for bad molecule/bond length
+        # combinations, so just delete here but don't raise error
+        # to avoid stopping other processes in generate_data.py.
+        logfile.unlink()
+    except Exception as e:
+        # Delete the log file and raise error to stop other processes.
+        logfile.unlink()
+        raise e
 
 
 if __name__ == "__main__":
